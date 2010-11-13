0676f5613061c870b74d900054d686d920a86190
Nagash <l.nagash@gmail.com>
2010-11-13 22:34:16 +0200

    added translate

---
+++ messages/ru/fp.php
@@ -0,0 +1,18 @@
+<?php
+return array(
+    'Seo tools' => 'SEO инструменты',
+    'Content' => 'Содержание',
+    'Yes' => 'Да',
+    'No' => 'Нет',
+	'Url' => 'URL',
+    'Title' => 'Название страницы',
+    'Keywords' => 'Ключевые слова',
+    'Description' => 'Описание страницы',
+    'Template Name' => 'Шаблон для страницы',
+    'Registration Required' => 'Регистрация обязательна',
+    'Update' => 'Сохранить',
+    'See page' => 'Посмотреть страницу'
+
+
+)
+?>
